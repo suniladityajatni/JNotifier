6c37a2ce75f915c5d89793b69480d6a82ee967ee
Aditya <suniladityajatni@gmail.com>
2022-09-27 03:52:11 +0530

    Fixed Typo and reduced 24 hrs to 6 hours

--- index.js
+++ index.js
@@ -176,6 +176,6 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static("frontend/build"))
     const path = require('path')
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirnmae, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
